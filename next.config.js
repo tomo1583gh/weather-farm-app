3a942321855cd36475ec2e11c4d50e2a43c5fb24
tomo1583 <tomo0523fx@yahoo.co.jp>
2025-11-21 15:23:11 +0900

    Fix GitHub Pages basePath

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  basePath: "/weather-farm-app",
+  assetPrefix: "/weather-farm-app",
+};
+
+export default nextConfig;
