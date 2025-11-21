6c1fa6dc1ed57e6de4c60354cab2b3c19947d1ef
tomo1583 <tomo0523fx@yahoo.co.jp>
2025-11-21 15:37:24 +0900

    Remove next.config.js and fix GitHub Pages build

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  basePath: "/weather-farm-app",
-  assetPrefix: "/weather-farm-app",
-};
-
-export default nextConfig;
